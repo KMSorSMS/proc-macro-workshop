a1e50671ed46f947cb6608d76c5750c19b337b3b
liam <liamyzw@126.com>
2024-08-10 15:58:06 +0800

    pass builder-01-parse.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,12 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _input = syn::parse_macro_input!(input as syn::DeriveInput);
+    // generate the builder code
+    let expanded = quote! {
+        // the generated code will go here
+    };
+    TokenStream::from(expanded)
 }
